3c8dcafcee15fb5e692d35118393dda7d0b01764
Andrey <y899yy001>
2022-04-23 01:07:04 +0300

    new Division by Two

---
+++ Home_Work/Home_Work10/Task02/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 2: Проверка на простое число:
+N = 13 -> "Это простое число"
+N = 12 -> “Это не простое число”*/
+
+int Promt(string massage)
+{
+    Console.WriteLine(massage);
+    int A = Convert.ToInt32(Console.ReadLine());
+    return A;
+}
+
+void Division(int N, int B)
+{
+    if (N / B == 1)
+    {
+        Console.WriteLine("Это простое число ");
+        return;
+    }
+    if (N % B != 0)
+    {
+        Division(N, B + 1);
+    }
+    else Console.WriteLine($" Это не простое число  ");
+}
+
+int A = Promt("Введите число: N ");
+Division(A, 2);
