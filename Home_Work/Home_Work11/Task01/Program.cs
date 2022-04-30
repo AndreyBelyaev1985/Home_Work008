0ff1a4b4d0587d961c483bc4af42342d45314d25
Andrey <y899yy001>
2022-04-30 20:46:36 +0300

    Заданы два массива info, data

--- Home_Work/Home_Work11/Task01/Program.cs
+++ Home_Work/Home_Work11/Task01/Program.cs
@@ -1,51 +1,50 @@
-﻿/* Задача №3 Заданы два массива: Info и data. В Массиве Info хранятся двоичные представления нескольких чисел (без разделителей).
-В массиве data хранится информация о колличестве бит, которые занимают числа из массива info. Напишите програму, которая составит 
-массив десятичных представленний чисел массива data с учётом информации из массива info.
+﻿/*
+Задача 3: 
+Заданы 2 массива: info и data. 
+В массиве info хранятся двоичные представления нескольких чисел (без разделителя). 
+В массиве data хранится информация о количестве бит, которые занимают числа из массива info. 
+Напишите программу, которая составит массив десятичных представлений чисел массива data 
+с учётом информации из массива info.
 
-Входные данные: 
+входные данные:
+    data = {0, 1, 1, 1, 1, 0, 0, 0, 1 }
+    info = {2, 3, 3, 1 }
 
-data = {0,1,1,1,1,0,0,0,1}
-info = {2,3,3,1}
+выходные данные:
+    1, 7, 0, 1
 
-Выходные данные:
-1,7,0,1. */
-
-void PrintArrya(int[] Mas)
-{
-    int count = Mas.Length;
-    for (int i = 0; i < count - 1; i++)
-    {
-        Console.Write(Mas[i] + ", ");
-
-    }
-    Console.WriteLine(Mas[Mas.Length - 1]);
-}
-
-void BinaryToDecimal(int[] data, int[] info, int begin = 0, int count = 0)
-{
-    if (begin >= data.Length)
-        return;
-    int c = info[count];
-    int temp = 0;
-    for (int i = 0; i < c; i++)
-    {
-        temp = temp + data[begin + c - i - 1] * Powering(2, i);
-    }
-    info[count] = temp;
-    BinaryToDecimal(data, info, begin + c, count + 1);
-}
-
-int Powering(int b, int a)
-{
-    if (a <= 0)
-        return 1;
-    return b * Powering(b, a - 1);
-}
-
+*/
 
 
 int[] data = { 0, 1, 1, 1, 1, 0, 0, 0, 1 };
 int[] info = { 2, 3, 3, 1 };
+int[] result = new int[info.Length];
+
+void BinaryToDecimal(int[] data, int[] info) // преобразуем из 2-ой в 10-ую 
+{
+    int k = 0;  // счетчик количества цифр в data
+
+    for (int i = 0; i < info.Length; i++)
+    {
+        for (int j = 0; j < info[i]; j++)
+        {
+            result[i] += data[k + j] * (int)Math.Pow(2, info[i] - 1 - j);
+
+        }
+        k += info[i];
+    }
+}
+
+void PrintArr(int[] Mas) // Вывод массива
+{
+    int count = Mas.Length;
+    for (int pos = 0; pos < count - 1; pos++)
+    {
+        Console.Write(Mas[pos] + "  ");
+    }
+    Console.WriteLine(Mas[Mas.Length - 1]);
+}
+
 
 BinaryToDecimal(data, info);
-PrintArrya(info);
+PrintArr(result);
